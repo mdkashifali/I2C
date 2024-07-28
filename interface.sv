a5bd4458f1b26ca3ef0d35373060e4f365a91d59
mdkashifali <69759421+mdkashifali@users.noreply.github.com>
2024-07-28 21:15:00 +0530

    Create interface.sv

---
+++ interface.sv
@@ -0,0 +1,13 @@
+interface i2c_if;
+  
+  logic clk;
+  logic rst;
+  logic newd;
+  logic op;   
+  logic [7:0] din;
+  logic [6:0] addr;
+  logic [7:0] dout;
+  logic  done;
+  logic busy, ack_err;  
+  
+endinterface
